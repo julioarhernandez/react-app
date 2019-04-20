3e8906296a0430fb835a661a156e2645bbb02b63
Julio <Julioarhernandez@gmail.com>
2019-04-20 19:53:21 -0400

    Change api server url

--- src/helpers/urlHelpers.js
+++ src/helpers/urlHelpers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 // const getBaseUrl = 'http://localhost:3001';
-const getBaseUrl = "https://www.dealby.us";
+const getBaseUrl = "https://api.dealby.us";
 
 export default getBaseUrl;
